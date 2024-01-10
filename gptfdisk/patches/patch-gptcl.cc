619859350f6bbee682077dc83e13b2a66791c1d4
kerrnel <greg-com-github@akua.com>
2024-01-10 16:30:52 -0500

    Pkg Lint
    Tweaks, warnings, inttypes.h

--- gptfdisk/patches/patch-gptcl.cc
+++ gptfdisk/patches/patch-gptcl.cc
@@ -2,9 +2,26 @@ $NetBSD$
 
 Maybe a no-op in progress to mutable device path
 
---- gptcl.cc.orig	2022-04-14 23:17:12.000000000 +0000
+--- gptcl.cc.orig	2022-04-14 19:17:12.000000000 -0400
 +++ gptcl.cc
-@@ -161,7 +161,8 @@ int GPTDataCL::DoOptions(int argc, char*
+@@ -65,7 +65,7 @@
+ // 8 = disk replication operation (-R) failed
+ int GPTDataCL::DoOptions(int argc, char* argv[]) {
+    GPTData secondDevice;
+-   int opt, numOptions = 0, saveData = 0, neverSaveData = 0;
++   int opt, saveData = 0, neverSaveData = 0;
+    int partNum = 0, newPartNum = -1, saveNonGPT = 1, retval = 0, pretend = 0;
+    int byteSwapPartNum = 0;
+    uint64_t low, high, startSector, endSector, sSize, mainTableLBA;
+@@ -151,7 +151,6 @@
+          default:
+             break;
+       } // switch
+-      numOptions++;
+    } // while
+ 
+    // Assume first non-option argument is the device filename....
+@@ -161,7 +160,8 @@
     if (device != NULL) {
        JustLooking(); // reset as necessary
        BeQuiet(); // Tell called functions to be less verbose & interactive
--- gptfdisk/patches/patch-gptcurses.cc
+++ gptfdisk/patches/patch-gptcurses.cc
@@ -1,42 +1,50 @@
 $NetBSD$
 
-Change to netbsd compiler friendliness lld -> lu, llx -> lux
+Use inttypes.h for printing 64 bit numbers compatibly
 
---- gptcurses.cc.orig	2022-04-14 23:17:12.000000000 +0000
+--- gptcurses.cc.orig	2022-04-14 19:17:12.000000000 -0400
 +++ gptcurses.cc
-@@ -333,13 +333,13 @@ void GPTDataCurses::ShowInfo(int partNum
+@@ -30,6 +30,7 @@
+ #endif
+ #include "gptcurses.h"
+ #include "support.h"
++#include <inttypes.h>
+ 
+ using namespace std;
+ 
+@@ -333,13 +334,13 @@
     printw("Partition GUID code: %s (%s)\n", partitions[partNum].GetType().AsString().c_str(),
            partitions[partNum].GetTypeName().c_str());
     printw("Partition unique GUID: %s\n", partitions[partNum].GetUniqueGUID().AsString().c_str());
 -   printw("First sector: %lld (at %s)\n", partitions[partNum].GetFirstLBA(),
-+   printw("First sector: %lu (at %s)\n", partitions[partNum].GetFirstLBA(),
++   printw("First sector: %" PRId64 " (at %s)\n", partitions[partNum].GetFirstLBA(),
            BytesToIeee(partitions[partNum].GetFirstLBA(), blockSize).c_str());
 -   printw("Last sector: %lld (at %s)\n", partitions[partNum].GetLastLBA(),
-+   printw("Last sector: %lu (at %s)\n", partitions[partNum].GetLastLBA(),
++   printw("Last sector: %" PRId64 " (at %s)\n", partitions[partNum].GetLastLBA(),
            BytesToIeee(partitions[partNum].GetLastLBA(), blockSize).c_str());
     size = partitions[partNum].GetLastLBA() - partitions[partNum].GetFirstLBA() + 1;
 -   printw("Partition size: %lld sectors (%s)\n", size, BytesToIeee(size, blockSize).c_str());
 -   printw("Attribute flags: %016llx\n", partitions[partNum].GetAttributes().GetAttributes());
-+   printw("Partition size: %lu sectors (%s)\n", size, BytesToIeee(size, blockSize).c_str());
-+   printw("Attribute flags: %016lux\n", partitions[partNum].GetAttributes().GetAttributes());
++   printw("Partition size: %" PRId64 " sectors (%s)\n", size, BytesToIeee(size, blockSize).c_str());
++   printw("Attribute flags: %016" PRIX64 "\n", partitions[partNum].GetAttributes().GetAttributes());
     #ifdef USE_UTF16
     partitions[partNum].GetDescription().extract(0, NAME_SIZE , temp, NAME_SIZE );
     printw("Partition name: '%s'\n", temp);
-@@ -447,7 +447,7 @@ void GPTDataCurses::MakeNewPart(void) {
+@@ -447,7 +448,7 @@
        clrtoeol();
        newFirstLBA = currentSpace->firstLBA;
        Align(&newFirstLBA);
 -      printw("First sector (%lld-%lld, default = %lld): ", newFirstLBA, currentSpace->lastLBA, newFirstLBA);
-+      printw("First sector (%lu-%lu, default = %lu): ", newFirstLBA, currentSpace->lastLBA, newFirstLBA);
++      printw("First sector (%" PRId64 "-%" PRId64 ", default = %" PRId64 "): ", newFirstLBA, currentSpace->lastLBA, newFirstLBA);
        echo();
        getnstr(inLine, 79);
        noecho();
-@@ -461,7 +461,7 @@ void GPTDataCurses::MakeNewPart(void) {
+@@ -461,7 +462,7 @@
     while ((newLastLBA > currentSpace->lastLBA) || (newLastLBA < newFirstLBA)) {
        move(LINES - 3, 0);
        clrtoeol();
 -      printw("Size in sectors or {KMGTP} (default = %lld): ", size);
-+      printw("Size in sectors or {KMGTP} (default = %lu): ", size);
++      printw("Size in sectors or {KMGTP} (default = %" PRId64 "): ", size);
        echo();
        getnstr(inLine, 79);
        noecho();
